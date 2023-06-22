9121b78b50cade9825237417bcd5921a8866582e
denichiro <87576918+Deijiro0138@users.noreply.github.com>
2023-06-23 01:08:45 +0900

    Scroll bar is added
    ChatHistory is shown in scroll bar

--- Assets/My_Assets/Scripts/ViewPrintManager.cs
+++ Assets/My_Assets/Scripts/ViewPrintManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using AAA.OpenAI;
+
+
 public class ViewPrintManager: MonoBehaviour
 {
     [SerializeField] string openAIApiKey;
